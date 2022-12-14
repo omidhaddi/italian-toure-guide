baa65d2b2554026135b8250e89dc5205e2d75102
OMID HADDI <113886529+omidhaddi@users.noreply.github.com>
2022-12-14 13:32:49 +0100

    Update 20221206105634-create-city.js

--- database/migrations/20221206105634-create-city.js
+++ database/migrations/20221206105634-create-city.js
@@ -24,15 +24,12 @@ module.exports = {
       metroPrice: {
         type: Sequelize.FLOAT
         },
-<<<<<<< HEAD
-=======
         lon: {
           type: Sequelize.FLOAT
         },
         lat: {
           type: Sequelize.FLOAT
         },
->>>>>>> main
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
