a59a19a7b827825fddc56706c0410952b69c7a6f
OMID HADDI <113886529+omidhaddi@users.noreply.github.com>
2022-12-14 13:44:08 +0100

    Update map.js

--- pages/routes/map.js
+++ pages/routes/map.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import Map from '../../components/Map'
-import Navbar from '../../components/Navbar'
-import placeController from '../../controllers/place'
-
-export default function ShowRoute({ places }) {
-=======
 import CityMap from '../../components/CityMap'
 import Navbar from '../../components/Navbar'
 import cityController from '../../controllers/city'
@@ -12,21 +5,11 @@ import styles from '../../styles/Card.module.css'
 import Search from '../../components/Search'
 
 export default function ShowRoute({ cities }) {
->>>>>>> main
 
     return (
         <>
             <div >
-<<<<<<< HEAD
-
-                <Map places={places} />
-            </div>
-            <br/>
-            <Map places={places} />
-            <br/>
-            <Map places={places} />
-=======
-                <Search style={{
+       <Search style={{
                     textAlign: 'center',
                     padding: '10px',
                     marginLeft: '40px',
@@ -37,7 +20,7 @@ export default function ShowRoute({ cities }) {
                 <CityMap cities={cities} />
             </div>
             <br />
->>>>>>> main
+
             <Navbar />
         </>
     )
@@ -45,17 +28,12 @@ export default function ShowRoute({ cities }) {
 
 export async function getServerSideProps(req, res) {
 
-<<<<<<< HEAD
-    const places = await placeController.all()
 
-    return {
-        props: { places },
-=======
     const cities = await cityController.all()
 
     return {
         props: { cities },
->>>>>>> main
+
     }
 
 }
